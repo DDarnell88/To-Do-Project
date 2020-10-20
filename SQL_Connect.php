a6886aa63c45f776b903f68ef86e54607b94d3e1
DDarnell88 <72808074+DDarnell88@users.noreply.github.com>
2020-10-19 23:33:40 -0500

    Update SQL_Connect.php

--- SQL_Connect.php
+++ SQL_Connect.php
@@ -14,13 +14,41 @@ if ($conn->connect_error) {
 $query = "SELECT * FROM `to do`";
 $result = $conn->query($query);
 
-if ($result->num_rows > 0) {
+//$data = $result->fetch_all();
+//echo json_encode($data);
+//return $data; 
+
+//if ($result->num_rows > 0) {
+if ($result!==false) {
+  $html_table = '<div class="table-responsive">
+				<table class="table table-dark">
+				<thead>
+				<tr>
+				<th scope="col">ID</th>
+				<th scope="col">To Do</th>
+				<th scope="col">Notes</th>
+				<th scope="col">Deadline</th>
+				</tr>
+				</thead>';
+  
   // output data of each row
-  while($row = $result->fetch_assoc($query)) {  
-  echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " Description: " . $row["Description"] . " Deadline: " . $row["Deadline"] . " <br>";
+  //while($row = $result->fetch_assoc()) { 
+  foreach($result as $row) {
+	  
+  //echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " Description: " . $row["Description"] . " Deadline: " . $row["Deadline"] . " <br>";
+  $html_table .= '<tbody>
+				  <tr>
+				  <th scope="row">' .$row['ID']. '</th>
+				  <td>' .$row['Name']. '</td>
+				  <td>' .$row['Description']. '</td>
+				  <td>' .$row['Deadline']. '</td>
+				  </tr>'; 
   }
 } else {
   echo "0 results";
 }
 $conn->close();
+$html_table .= '</tbody></table></div>';
+
+echo $html_table;
 ?>
