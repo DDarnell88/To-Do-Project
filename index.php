4c03d1784bc3ccebaf2bacef173e8e6c7126f935
DDarnell88 <72808074+DDarnell88@users.noreply.github.com>
2020-10-19 23:35:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
+<?php
+//$data = require_once('SQL_Connect.php');
+//var_dump($data);
+?>
+
 <!DOCTYPE html>
 <html>
-<head><title>PHP TO-DO List</title></head>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+	<title>PHP TO-DO List</title>
+</head>
 
 <body>
+<div class="container">
 <h1>PHP TO-DO List</h1>
 
 <form action="To_Do.php" method="post">
@@ -12,14 +24,13 @@ Deadline: <input type="date" name="Deadline"><br>
 
 Note: <input type="text" name="Note"><br>
 
-<input class=" type="submit">
-</form> 
+<input class="" type="submit">
 
-<?php
-$SQL_C = fopen("SQL_Connect.php", "r");
-echo $SQL_C;
-fclose($SQL_C);
-?>
+</form> 
+<br>
+<?php include "SQL_Connect.php"; ?>
+
+</div>
 </body>
 
 </html>
