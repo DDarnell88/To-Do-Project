3466206ab3098d676a1fe256fd3131be2a441482
DDarnell88 <72808074+DDarnell88@users.noreply.github.com>
2020-10-27 13:34:12 -0500

    Update update.php

--- update.php
+++ update.php
@@ -1,4 +1,11 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+		// all is good
+	} else {
+		
+		exit('Invalid Request');
+	}
+	
 $conn = require_once 'SQL_Connect.php';	
 $id = $_POST['id'];
 //var_dump($_POST);
@@ -6,10 +13,26 @@ $id = $_POST['id'];
 	$Name = mysqli_real_escape_string($conn,$_POST['To_Do']);
 	$Description = mysqli_real_escape_string($conn, $_POST['Description']);
 	$Deadline = mysqli_real_escape_string($conn, $_POST['Deadline']);
-
-	$update = "UPDATE`to do` 
-			SET Name='$Name', Description='$Description', Deadline='$Deadline' 
-			WHERE ID ='".$id."'";
+	$fields = array($Name, $Description, $Deadline);
+	$semaphore = false;
+	$update = "UPDATE`to do` SET ";
+	
 	$result = $conn->query($update);
+	foreach ($fields as $field) {
+		if (isset($_POST[$field] and !empty($_POST[$field]))) { //Getting the following error code here: Fatal error: Cannot use isset() on the result of an expression (you can use "null !== expression" instead) in C:\xampp\htdocs\To_Do_List\update.php on line 22
+			$var = mysql_real_escape_string($_POST[$field]);
+			$update .= uppercase($field) . " = '$var'";
+			$semaphore = true;
+		}
+		
+	}
+if ($semaphor) {
+	$update .= "WHERE ID ='".$id."'";
+}
+
+	 
+			//Name='$Name', Description='$Description', Deadline='$Deadline' 
+			
+	
 header("Location: index.php");	
 ?>
