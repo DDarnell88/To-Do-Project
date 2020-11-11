0c0c1c82f64e43cd9259ff8d46cfd4daa95adbe8
DDarnell88 <72808074+DDarnell88@users.noreply.github.com>
2020-11-11 15:00:29 -0600

    Update formHandle.js

--- formHandle.js
+++ formHandle.js
@@ -1,25 +1,16 @@
-/* attach a submit handler to the form */
-$("#form").submit(function(event) {
-
-  /* stop form from submitting normally */
-  event.preventDefault();
-
-  /* get the action attribute from the <form action=""> element */
-  var $form = $(this),
-    url = $form.attr('action');
-
-  /* Send the data using post with element id name and name2*/
-  var posting = $.post(url) {
-    ToDo: $('#To_DO').val(),
-    Description: $('#Description').val()
-    Deadline: $('#Deadline').val()
-  });
-
-  /* Alerts the results */
-  posting.done(function(data) {
-    $('#result').text('success');
-  });
-  posting.fail(function() {
-    $('#result').text('failed');
-  });
+$(document).ready(function(){
+    $('#form').submit(function(event){
+        event.preventDefault();
+        $.ajax({
+            url: "To_Do.php",
+            type: "POST",
+            data: $(this).serialize(),
+            success: function(data){
+                $("#result").html(data);
+            },
+            error: function(){
+                alert("Form submission failed!");
+            }
+        });
+    });
 });
